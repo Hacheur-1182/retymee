2d07ae19d169a0d3b3d8649d1334dfc320412113
kevin-it237 <ngaleuabel@gmail.com>
2020-04-02 18:06:32 +0100

    Changer colors

--- functions/registration_mail.js
+++ functions/registration_mail.js
@@ -1,6 +1,6 @@
 module.exports = function (email, username) {
 	const nodemailer = require('nodemailer');
-	var data = '<br><center><a style="color:rgb(234, 58, 60);border: 1px solid rgb(234, 58, 60);font-size:30px;background-color:white;padding:.5em 3em;text-decoration:none" href="http://retymee.com/student/'+username+'/activateAccount">Activate your Account</a></center>'
+	var data = '<br><center><a style="color:rgb(234, 58, 60);border: 1px solid rgb(234, 58, 60);font-size:30px;background-color:white;padding:.5em 3em;text-decoration:none" href="localhost:3000/student/'+username+'/activateAccount">Activate your Account</a></center>'
 	+'<br><h1 style="color:black;text-align:center">Merci de vous être inscrit</h1>'
 	+'<h3 style="color:black;text-align:center">Vous devez activer votre compte pour pouvoir accéder à votre profile.</h3>'
 	+'<p><center>Cliquez sur le bouton activer ci-dessus.<center></p>'
