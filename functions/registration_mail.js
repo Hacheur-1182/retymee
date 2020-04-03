51554ffc9c2752d39a7c57220b30b6bada9dced4
kevin-it237 <ngaleuabel@gmail.com>
2020-04-03 18:54:09 +0100

    Make responsive 4

--- functions/registration_mail.js
+++ functions/registration_mail.js
@@ -1,8 +1,8 @@
 module.exports = function (email, username) {
 	const nodemailer = require('nodemailer');
-	var data = '<br><center><a style="color:rgb(234, 58, 60);border: 1px solid rgb(234, 58, 60);font-size:30px;background-color:white;padding:.5em 3em;text-decoration:none" href="http://retymee.com/student/'+username+'/activateAccount">Activate your Account</a></center>'
-	+'<br><h1 style="color:black;text-align:center">Merci de vous être inscrit</h1>'
-	+'<h3 style="color:black;text-align:center">Vous devez activer votre compte pour pouvoir accéder à votre profile.</h3>'
+	var data = '<br><center><a style="color:#004368;border: 1px solid #004368;font-size:21px;background-color:white;padding:.5em 3em;text-decoration:none" href="http://retymee.com/student/'+username+'/activateAccount">Activate your Account</a></center>'
+	+'<br><h3 style="color:black;text-align:center">Merci de vous être inscrit</h3>'
+	+'<h5 style="color:black;text-align:center">Vous devez activer votre compte pour pouvoir accéder à votre profile.</h5>'
 	+'<p><center>Cliquez sur le bouton activer ci-dessus.<center></p>'
 	+'<br><p style="text-align:center">Copyright © 2020. Powered by Inchtechs</p>';
     let transporter = nodemailer.createTransport({
