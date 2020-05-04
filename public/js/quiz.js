ea7aea5ad4a03fc7e51beeda1fa0374cdc96dfab
kevin-it237 <ngaleuabel@gmail.com>
2020-05-04 20:51:21 +0100

    Add quiz results print

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -124,4 +124,30 @@ $(document).ready(function() {
             </div>`
         )
     })
+
+    // Imprimer les résultats
+    $('#print').click(function() {
+        var sTable = document.getElementById('pagecontent').innerHTML;
+
+        var style = "<style>";
+        style = style + "table {width: 100%;font: 17px Calibri;}";
+        style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+        style = style + "padding: 2px 3px;text-align: center;}";
+        style = style + "</style>";
+
+        // CREATE A WINDOW OBJECT.
+        var win = window.open('', '', 'height=700,width=700');
+
+        win.document.write('<html><head>');
+        win.document.write('<title>Evaluation</title>');   // <title> FOR PDF HEADER.
+        win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
+        win.document.write('</head>');
+        win.document.write('<body>');
+        win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
+        win.document.write('</body></html>');
+
+        win.document.close(); 	// CLOSE THE CURRENT WINDOW.
+
+        win.print();    // PRINT THE CONTENTS.
+    })
 })
